8663e1003acfef33975c7270edae38e5e6b2100c
Kevin Gray <graykevinb@gmail.com>
2023-10-30 08:16:02 -0500

    Not working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { setState } from 'react';
+import Board from './Board';
+export default function App() {
+  const [player] = React.useState('X');
+  setState({player: player === 'X' ? 'O' : 'X'});
+    return (
+        <div className="App">
+            <Board size={3}></Board>
+        </div>
+    );
 }
-
-export default App;
